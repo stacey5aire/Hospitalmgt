375dd7afe228b69b9a018363a18516cf7d2b20c1
ssenyonjoalvin <alvinssenyonjo9@gmail.com>
2024-04-12 15:53:24 +0300

    Revert "user interface and admin dashboard"
    This reverts commit dfa1257e2678147da37bcee8982995fede1fd283.

--- hospital-management-system/routes/web.php
+++ hospital-management-system/routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
- 
-Route::get('/', [HomeController::class,'index' ] );
- //Route::get('/loggedin',[HomeController::class,'index'])  ; 
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::middleware([
     'auth:sanctum',
